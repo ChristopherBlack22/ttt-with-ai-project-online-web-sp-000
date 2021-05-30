47dca0fcdaa7df4be2ace073effd37077b297b6a
Christopher Black <christopher.black.2209@googlemail.com>
2021-05-30 23:34:19 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,12 +3,13 @@ module Players
   
     def move(board)
       input = nil 
-      if !board.taken?(5)
+      if !board.taken?("5")
         input = "5"
-      elsif !board.taken?(1)
-        "1"
-      elsif !board.taken?(3)
-        
+      elsif !board.taken?("1")
+        inout = "1"
+      elsif !board.taken?("3")
+        input = "3"
+      elsif !board.taken?("7") 
       end 
       input
     end 
