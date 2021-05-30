78d3539ac7ad96fdef894ce4ec3a28893fcd775c
Christopher Black <christopher.black.2209@googlemail.com>
2021-05-30 23:49:57 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,15 +6,21 @@ module Players
       if !board.taken?("5")
         input = "5"
       elsif !board.taken?("1")
-        inout = "1"
+        input = "1"
       elsif !board.taken?("3")
         input = "3"
       elsif !board.taken?("7")
         input = "7"
       elsif !board.taken?("9")
         input = "9"
-      elsif !board.taken?
-      input = 
+      elsif !board.taken?("2")
+        input = "2"
+      elsif !board.taken?("4")
+        input = "4"
+      elsif !board.taken?("6")
+        input = "6"
+      elsif !board.taken?("8")
+        input = "8"
       end 
       input
     end 
