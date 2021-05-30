4aec517b6e6c82fb95a46812bfe9eae54808fc14
Christopher Black <christopher.black.2209@googlemail.com>
2021-05-30 23:12:34 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,7 @@ class Game
   def won?
     WIN_COMBINATIONS.detect do |combo|
       @board.cells[combo[0]] == @board.cells[combo[1]] &&
-      @board.cells[combo[1]] == @board.cells[combo[2]] && @board.position()
+      @board.cells[combo[1]] == @board.cells[combo[2]] && @board.position(combo[1])
       #@board.taken?(combo[0]+1)
     end
   end
